2324d29f33aaf971cb26f5bdda23d658e8b5e940
Tom-Brindley <80006438+Tom-Brindley@users.noreply.github.com>
2021-03-03 21:23:15 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+let myAge = 30;
+//my age variable set to 30
+let earlyYears = 2;
+//early years are a ratio of 1:10.5
+earlyYears = 10.5 * earlyYears;
+let laterYears= myAge - 2;
+//variable laterYears are multiplied by four and occur from age 3 onwards.
+laterYears *= 4;
+//assign lateryears to dog lateryears
+console.log(earlyYears);
+console.log(laterYears);
+myAgeInDogYears = earlyYears + laterYears;
+//my combined age converterd to dog years
+console.log(myAgeInDogYears);
+myName = 'Thomas Brindley'.toLowerCase();
+//prints myname in lower case
+console.log(myName);
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
